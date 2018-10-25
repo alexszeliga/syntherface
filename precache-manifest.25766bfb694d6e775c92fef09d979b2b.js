4ec10bc4e4da02dbf376d23a7a764c02ab3bef7e
alexszeliga <alexszeliga@gmail.com>
2018-10-25 10:43:34 -0400

    Updates

---
+++ precache-manifest.25766bfb694d6e775c92fef09d979b2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18df7c9b14c0d0bf615f",
+    "url": "/guitar-knob/static/js/runtime~main.18df7c9b.js"
+  },
+  {
+    "revision": "ef544f4a3633b4ea8187",
+    "url": "/guitar-knob/static/js/main.ef544f4a.chunk.js"
+  },
+  {
+    "revision": "b2fd0420134aa70449ee",
+    "url": "/guitar-knob/static/js/1.b2fd0420.chunk.js"
+  },
+  {
+    "revision": "ef544f4a3633b4ea8187",
+    "url": "/guitar-knob/static/css/main.0b60a32f.chunk.css"
+  },
+  {
+    "revision": "640ed60f7be7ad62f4eec9d923feea31",
+    "url": "/guitar-knob/index.html"
+  }
+];
