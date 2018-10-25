e9b590de588544f4293655fa3fa95508edf14dac
alexszeliga <alexszeliga@gmail.com>
2018-10-25 11:02:09 -0400

    Updates

---
+++ precache-manifest.3548010b2051a154360eb7e7a90fd956.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a04804aa6ebf2e24eb2a",
+    "url": "/syntherface/%20origin/static/js/runtime~main.a04804aa.js"
+  },
+  {
+    "revision": "8e88ae80a2d8c85b4e06",
+    "url": "/syntherface/%20origin/static/js/main.8e88ae80.chunk.js"
+  },
+  {
+    "revision": "b2fd0420134aa70449ee",
+    "url": "/syntherface/%20origin/static/js/1.b2fd0420.chunk.js"
+  },
+  {
+    "revision": "8e88ae80a2d8c85b4e06",
+    "url": "/syntherface/%20origin/static/css/main.0b60a32f.chunk.css"
+  },
+  {
+    "revision": "140640e38f94d19a3e78b512b6cc18ac",
+    "url": "/syntherface/%20origin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/guitar-knob/precache-manifest.25766bfb694d6e775c92fef09d979b2b.js"
+  "/syntherface/%20origin/precache-manifest.3548010b2051a154360eb7e7a90fd956.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/guitar-knob/index.html", {
+workbox.routing.registerNavigationRoute("/syntherface/%20origin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
